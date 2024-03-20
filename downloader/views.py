957d5c997acbc3af069aff07ea693b2a1b5dea5f
sabuhi.isazade@yahoo.com <sabuhi.isazade@yahoo.com>
2024-03-20 12:07:16 +0100

    final:creating views and template

--- downloader/views.py
+++ downloader/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from pytube import YouTube  # Make sure to install pytube with pip install pytube
 
-# Create your views here.
+def download_video(request):
+    context = {}
+    if request.method == 'POST':
+        video_url = request.POST.get('url', '')
+        if video_url:
+            yt = YouTube(video_url)
+            download_directory = "C:/Users/sabuh/Projects"
+            yt.streams.get_highest_resolution().download(output_path=download_directory)
+            context['message'] = 'Download completed!'
+            context['download_path'] = download_directory
+    return render(request, 'downloader/download_form.html', context)
